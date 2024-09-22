c3151958043f172c7e5f2b61f6e7325113609178
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-09-22 21:47:50 +0530

    Create socket server and add socket types

--- server.ts
+++ server.ts
@@ -1,17 +1,56 @@
 import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
+import { Server } from 'socket.io';
+import {
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from '@/types/socketTypes';
 
+const hostname = 'localhost';
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  const httpServer = createServer((req, res) => {
     const parsedUrl = parse(req.url!, true);
     handle(req, res, parsedUrl);
-  }).listen(port);
+  });
+
+  const io = new Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+  >(httpServer);
+
+  io.on('connection', socket => {
+    socket.on('hello', () => {
+      console.log('Hello received');
+    });
+
+    socket.emit('noArg');
+    socket.emit('basicEmit', 1, '2', Buffer.from([3]));
+    socket.emit('withAck', '4', e => {
+      console.log(`Ack received with value: ${e}`);
+    });
+
+    socket.data.name = 'John';
+    socket.data.age = 42;
+  });
+
+  httpServer
+    .once('error', err => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
 
   console.log(
     `> Server listening at http://localhost:${port} as ${
