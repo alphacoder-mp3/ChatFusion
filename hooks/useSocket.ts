21eca61d84b15197c4c3ccf8a41a6da670d47fee
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-10-05 15:58:54 +0530

    Add socket hook with typescript

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,11 +1,29 @@
 import { useEffect, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
 
+interface ServerToClientEvents {
+  message: (message: { sender: string; content: string }) => void;
+  typing: (data: { username: string }) => void;
+  'stop typing': (data: { username: string }) => void;
+}
+
+interface ClientToServerEvents {
+  message: (message: { content: string }) => void;
+  typing: () => void;
+  'stop typing': () => void;
+}
+
 export const useSocket = () => {
-  const [socket, setSocket] = useState<Socket | null>(null);
+  const [socket, setSocket] = useState<Socket<
+    ServerToClientEvents,
+    ClientToServerEvents
+  > | null>(null);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:3000');
+    const token = localStorage.getItem('token'); // Assume you store the JWT in localStorage
+    const newSocket = io('http://localhost:3000', {
+      auth: { token },
+    });
     setSocket(newSocket);
 
     return () => {
