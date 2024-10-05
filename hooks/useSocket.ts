be44d1963daa56dee021f5153019aeb73b77d467
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-10-05 15:54:36 +0530

    Add custom hook usesocket for using socket

---
+++ hooks/useSocket.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
+
+export const useSocket = () => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:3000');
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
+  return { socket };
+};
